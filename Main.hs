39b1968b8bb648ce398ef02d8513eaf0d7b76d4d
yuanwang <yuan.wang@workiva.com>
2021-11-30 11:16:21 -0600

    update

--- Main.hs
+++ Main.hs
@@ -1,18 +1,27 @@
 module Main where
 
+import Control.Monad
 import Control.Monad.ST
 import Data.STRef
-import Control.Monad
-import HaskellSay (haskellSay)
+import qualified Data.Vector as V
 
 sumST :: Num a => [a] -> a
 sumST xs = runST $ do
   n <- newSTRef 0
-  forM_  xs $ \x -> do
-    modifySTRef n (+x)
+  forM_ xs $ \x -> do
+    modifySTRef n (+ x)
   readSTRef n
 
+quickSort :: Ord a => [a] -> [a]
+quickSort [] = []
+quickSort [x] = [x]
+quickSort (x : xs) = quickSort small ++ mid ++ quickSort large
+  where
+    small = [y | y <- xs, y < x]
+    mid = [y | y <- xs, y == x] ++ [x]
+    large = [y | y <- xs, y > x]
 
 main :: IO ()
 main = do
-  haskellSay $ "Hello Haskell Nixers!" <> show (sumST [1, 2, 3])
+  print (sumST [1, 2, 3])
+  print $ quickSort [3, 4, 0, 1, 8, 9, 10]
